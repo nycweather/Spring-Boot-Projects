0d53241a34df2968e00b5c524dd8fcf8eda80d6c
nycweather <alifftw@gmail.com>
2023-04-20 00:08:33 -0400

    update

--- Project-3-More-APIs/src/main/java/com/example/employeeapi/repository/EmployeeRepository.java
+++ Project-3-More-APIs/src/main/java/com/example/employeeapi/repository/EmployeeRepository.java
@@ -1,4 +1,8 @@
 package com.example.employeeapi.repository;
 
-public class EmployeeRepository {
+import com.example.employeeapi.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
