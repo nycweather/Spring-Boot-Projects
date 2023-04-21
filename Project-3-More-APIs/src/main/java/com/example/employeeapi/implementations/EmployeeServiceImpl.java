88d7f1a683d968243c121eaf800d69b0dea8a671
nycweather <alifftw@gmail.com>
2023-04-20 20:43:47 -0400

    Added postmapping

--- Project-3-More-APIs/src/main/java/com/example/employeeapi/implementations/EmployeeServiceImpl.java
+++ Project-3-More-APIs/src/main/java/com/example/employeeapi/implementations/EmployeeServiceImpl.java
@@ -1,6 +1,24 @@
 package com.example.employeeapi.implementations;
 
+import com.example.employeeapi.models.Employee;
+import com.example.employeeapi.repository.EmployeeRepository;
 import com.example.employeeapi.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        super();
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
